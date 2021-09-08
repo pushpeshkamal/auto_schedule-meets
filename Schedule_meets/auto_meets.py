23541449c54a996abdd70175d5fb3bc343dd2fa2
Pushpesh Kamal <pushpeshkamal3@gmail.com>
2021-09-08 23:27:56 +0530

    scheduled final

--- Schedule_meets/auto_meets.py
+++ Schedule_meets/auto_meets.py
@@ -13,12 +13,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 #installing webdrivers of chrome
+
 driver_path = "/Users/pushpesh/auto_mac/chromedriver"
 driver = webdriver.Chrome(driver_path)
 
 #loading the data reuired as the credentials
-email="pk1459@srmist.edu.in"
-pas="Rasu@2001"
+
+email="pls load your email id out here"
+pas="pls load your pssword over here"
 
 #disabling notifications, alers by using experimental features
 
@@ -40,7 +42,7 @@ driver = webdriver.Chrome(options= opt , executable_path='/Users/pushpesh/auto_m
 
 
 #login URL
-#driver.get("https://accounts.google.com/signin/v2/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2F%3Femr%3D0&followup=https%3A%2F%2Fclassroom.google.com%2F%3Femr%3D0&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
+
 driver.get("https://accounts.google.com/signin/v2/identifier?ltmpl=meet&continue=https%3A%2F%2Fmeet.google.com%3Fhs%3D193&&o_ref=https%3A%2F%2Fmeet.google.com%2F_meet%2Fwhoops%3Fsc%3D232%26alias%3Dmymeetingraheel&_ga=2.262670348.1240836039.1604695943-1869502693.1604695943&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
 
 email_input =driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")
@@ -55,34 +57,53 @@ time.sleep(2)
 
 #scheduled meets 
 
-link = open("meetlink.txt","r").read()
+link = open("meetlink.txt","r").read()#pls do write down the meeting link out here
 #print(link)
 
 
-driver.get(link)
+driver.get(link) #or just put your meet link out here
+
 #link_input = driver.find_element_by_xpath("/html/body/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[1]/label/input")
 #link_input.send_keys("fnq-orad-fwv")
 #time.sleep(2)
 press('enter')
 time.sleep(2)
 
+# for muting mic
+# for mac users pls use system preferces configured for vscode and google chrome to access
 press('tab')
 press('tab')
 press('tab')
 press('tab')
 press('enter')
 
+#alternative for mic off
 #micoff= driver.find_element_by_xpath("/html/body/div/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div/span/span/div/div[1]/div/svg")
 #micoff().click()
 time.sleep(2)
 
-#join now / ask to join
+#for pausing video (as of for other than mac) 
+#for macs by default system prefernce blocks it up
+#press('tab')
+#press('tab')
+#press('tab')
+#press('tab')
+#press('enter')
+
+#alternative for mic off
+#micoff= driver.find_element_by_xpath("/html/body/div/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div/span/span/div/div[1]/div/svg")
+#micoff().click()
+#time.sleep(2)
+
+#join now / ask to join 
 press('tab')
 press('tab')
 press('tab')
 press('tab')
 press('tab')
 press('enter')
+
+#alternative for join now or ask to join button
 #joinnow = driver.find_element_by_xpath("/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/span/span")
 #joinnow.click()
 time.sleep(2)
